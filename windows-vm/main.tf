7b032fcaaa6b1ea818fb87d2199ebbbd7e30c302
rejackson73 <28791872+rejackson73@users.noreply.github.com>
2020-05-22 12:48:17 -0400

    working setupclient.ps1 without servers

--- windows-vm/main.tf
+++ windows-vm/main.tf
@@ -13,7 +13,7 @@ data "azurerm_resource_group" "main-rg" {
 }
 
 data "azurerm_image" "search" {
-  name                = "rjackson-windows-0.03"
+  name                = "rjackson-windows-0.05"
   resource_group_name = data.azurerm_resource_group.main-rg.name
 }
 
@@ -52,8 +52,6 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
-
-
 resource "azurerm_windows_virtual_machine" "example" {
   name                = "${var.prefix}-Machine"
   resource_group_name = data.azurerm_resource_group.main-rg.name
@@ -62,6 +60,7 @@ resource "azurerm_windows_virtual_machine" "example" {
   source_image_id     = data.azurerm_image.search.id
   admin_username      = "adminuser"
   admin_password      = "P@$$w0rd1234!"
+  custom_data = "JE1ldGFEYXRhSGVhZGVycyA9IEB7Ik1ldGFkYXRhIj0idHJ1ZSJ9IApJbnZva2UtUmVzdE1ldGhvZCAtTWV0aG9kIEdFVCAtdXJpICJodHRwOi8vMTY5LjI1NC4xNjkuMjU0L21ldGFkYXRhL2luc3RhbmNlL25ldHdvcmsvaW50ZXJmYWNlLzAvaXB2NC9pcGFkZHJlc3MvMC9wdWJsaWNpcD9hcGktdmVyc2lvbj0yMDE3LTAzLTAxJmZvcm1hdD10ZXh0IiAtSGVhZGVycyAkTWV0YURhdGFIZWFkZXJzIAoKTmV3LUl0ZW0gJ0M6XGhhc2hpY29ycFxub21hZC5kXG5vbWFkLWNsaWVudC5oY2wnClNldC1Db250ZW50ICdDOlxoYXNoaWNvcnBcbm9tYWQuZFxub21hZC1jbGllbnQuaGNsJyAnYWR2ZXJ0aXNlIHsnIApTZXQtQ29udGVudCAnQzpcaGFzaGljb3JwXG5vbWFkLmRcbm9tYWQtY2xpZW50LmhjbCcgJyAgIGh0dHA9IiRjbGllbnRfcHVibGljX0lQIicKU2V0LUNvbnRlbnQgJ0M6XGhhc2hpY29ycFxub21hZC5kXG5vbWFkLWNsaWVudC5oY2wnICcgICBycGM9IiRjbGllbnRfcHVibGljX0lQIicKU2V0LUNvbnRlbnQgJ0M6XGhhc2hpY29ycFxub21hZC5kXG5vbWFkLWNsaWVudC5oY2wnICcgICBzZXJmPSIkY2xpZW50X3B1YmxpY19JUCInClNldC1Db250ZW50ICdDOlxoYXNoaWNvcnBcbm9tYWQuZFxub21hZC1jbGllbnQuaGNsJyAnYWR2ZXJ0aXNlIH0nCg=="
   network_interface_ids = [
     azurerm_network_interface.main.id,
   ]
@@ -78,23 +77,32 @@ resource "azurerm_windows_virtual_machine" "example" {
 }
 
 resource null_resource "winrm_provisioner" {
-  // provisioner "file" {
-  //   source = "setupclient.ps1"
-  //   destination = "c:\\hashicorp\\setupclient.ps1"
-  // }
+  provisioner "file" {
+    source = "setupclient.ps1"
+    destination = "c:\\hashicorp\\setupclient.ps1"
+  }
   provisioner "remote-exec" {
     inline = [
-      "New-Item 'C:/hashicorp/nomad.d/nomad-client.hcl'"
-      // "powershell -ExecutionPolicy Unrestricted -File  c:\\hashicorp\\setupclient.ps1 -client_public_IP ${azurerm_public_ip.main-ip.ip_address}"
+      // "New-Item 'C:/hashicorp/nomad.d/nomad-client.hcl'"
+      "powershell -ExecutionPolicy Unrestricted -File  c:\\hashicorp\\setupclient.ps1 -client_public_IP ${azurerm_public_ip.main-ip.ip_address}"
     ]
   }
+  // provisioner "remote-exec" {
+  //   inline = [
+  //     "powershell New-Item 'C:/robwashere.txt'"
+  //   ]
+  // }
   connection {
     host = azurerm_windows_virtual_machine.example.public_ip_address
     port = "5985"
     type = "winrm"
     user = azurerm_windows_virtual_machine.example.admin_username
     password = azurerm_windows_virtual_machine.example.admin_password
-    insecure = true
+    insecure = false
     https = false
   }
+}
+
+output "host_ip" {
+  value = azurerm_windows_virtual_machine.example.public_ip_address
 }
--- windows-vm/setupclient.ps1
+++ windows-vm/setupclient.ps1
@@ -1,9 +1,13 @@
 $MetaDataHeaders = @{"Metadata"="true"} 
-Invoke-RestMethod -Method GET -uri "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipaddress/0/publicip?api-version=2017-03-01&format=text" -Headers $MetaDataHeaders 
+$client_IP = Invoke-RestMethod -Method GET -uri "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipaddress/0/publicip?api-version=2017-03-01&format=text" -Headers $MetaDataHeaders 
 
-New-Item 'C:\hashicorp\nomad.d\nomad-client.hcl'
-Set-Content 'C:\hashicorp\nomad.d\nomad-client.hcl' 'advertise {' 
-Set-Content 'C:\hashicorp\nomad.d\nomad-client.hcl' '   http="$client_public_IP"'
-Set-Content 'C:\hashicorp\nomad.d\nomad-client.hcl' '   rpc="$client_public_IP"'
-Set-Content 'C:\hashicorp\nomad.d\nomad-client.hcl' '   serf="$client_public_IP"'
-Set-Content 'C:\hashicorp\nomad.d\nomad-client.hcl' 'advertise }'
+New-Item C:\hashicorp\nomad.d\nomad-client.hcl
+
+@"
+advertise {
+   http='$client_IP'
+   rpc='$client_IP'
+   serf='$client_IP'
+advertise }
+"@ > C:\hashicorp\nomad\nomad.d\nomad-client.hcl 
+
